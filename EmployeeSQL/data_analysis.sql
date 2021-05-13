af9e0d9bc3cdff9fefa0c04a29810b8f01b6a48a
Danielle Martin <danielle.jaclyn.martin@gmail.com>
2021-05-13 00:27:37 -0700

    query for department managers

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -5,8 +5,27 @@ FROM Employees
 JOIN Salaries
 USING (emp_no);
 
--- Find employees hired in 1986
+-- Find employees hired in 1986:
 
 SELECT first_name, last_name, hire_date
 FROM Employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+-- (Inner) Join Employees, Dept_manager, and Departments tables to query manager information:
+
+SELECT dept_no, dept_name, emp_no, last_name, first_name
+FROM Dept_manager
+JOIN Departments
+USING (dept_no)
+JOIN Employees
+USING (emp_no)
+
+-- Same query as above restricted to find the last/current manager:
+
+SELECT dept_no, dept_name, emp_no, last_name, first_name
+FROM Dept_manager
+JOIN Departments
+USING (dept_no)
+JOIN Employees
+USING (emp_no)
+WHERE to_date = '9999-01-01'
